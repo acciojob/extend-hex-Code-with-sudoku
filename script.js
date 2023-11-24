4af839f8b82c3d728c85246190217a9d0042e84f
Code-with-sudoku <137153314+Code-with-sudoku@users.noreply.github.com>
2023-11-24 11:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const hexWithoutHash = shortHex.startsWith('#') ? shortHex.slice(1) : shortHex;
+
+   
+    const fullHex = hexWithoutHash
+        .split('')
+        .map(char => char + char)
+        .join('');
+
+    return '#' + fullHex;
 };
 
 // Do not change the code below.
